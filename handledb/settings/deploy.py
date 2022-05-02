9a35467d9d2f53af3364e31d03b89a427923e593
ridebus82 <ridebus82@nate.com>
2022-05-03 02:58:20 +0900

    alskdjflafd

---
+++ handledb/settings/deploy.py
@@ -0,0 +1,40 @@
+
+
+from .base import *
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# False if not in os.environ because of casting above
+DEBUG = True
+
+# Raises Django's ImproperlyConfigured
+# exception if SECRET_KEY not in os.environ
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+SECRET_KEY = env('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'handledb',
+        'USER': 'pcyadmin',
+        'PASSWORD': "rkwkrh13!#",
+        'HOST': 'mariadb',
+        'PORT': '3306',
+    }
+}
