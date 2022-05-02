06e5dee9eb6fff3e6a308514fe9cd6d4b7129e42
ridebus82 <ridebus82@nate.com>
2022-05-03 05:59:58 +0900

    aldsikjfalsjdf

--- handledb/context_processors.py
+++ handledb/context_processors.py
@@ -7,8 +7,7 @@ from dbmanageapp.models import DbSetting
 def message_processor(request):
     base_item = DbSetting.objects.last()
 
-    if base_item.logo_image:
-        base_logo = base_item.logo_image
+    base_logo = base_item.logo_image
     base_company_name = base_item.company_name
     base_time = timezone.now().strftime ("%m%d%M%S")
     base_theme = base_item.theme_status
