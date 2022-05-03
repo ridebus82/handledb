56470f0794edb9e63ba656eb423d7b0765c64583
ridebus82 <ridebus82@nate.com>
2022-05-03 14:41:29 +0900

    asldjflasjdf

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.signin, name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', views.testpage, name='testpage'),
 ]
